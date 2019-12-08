e1266dd36984b6fe0ab635b9664a7ab384cbab84
dream_kid <sourovchakraborty420@gmail.com>
2019-12-08 18:32:00 +0600

    new commit

--- checkWord.py
+++ checkWord.py
@@ -1,14 +1,34 @@
-import json
-import time
-import sys
-def func(word)
-fo = open('words_dictionary.json','r')
-data = json.load(fo)
-searchKey = 'if'
-startTime=time.time()
-if searchKey in data.keys():
-	print("Match Found!")
-else:
-	print("Not found")
-#print("No.of words: ",len(data.keys()))
-#print("Run Time: ",time.time()-startTime,' seconds')
+from tkinter import *
+import PIL
+from PIL import ImageTk
+
+root = Tk()
+
+
+class PokemonClass(object):
+
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.WelcomeLabel = Label(root, text = "Welcome! Pick your Pokemon!", bg = "Black", fg = "White")
+        self.WelcomeLabel.pack(fill = X)
+
+        self.CharButton = Button(root, text = "Charmander", bg = "RED", fg = "White", command = CharClick)
+        self.CharButton.pack(side = LEFT, fill = X)
+
+        self.SquirtButton = Button(root, text = "Squirtle", bg = "Blue", fg = "White")
+        self.SquirtButton.pack(side = LEFT, fill = X)
+
+        self.BulbButton = Button(root, text = "Bulbasaur", bg = "Dark Green", fg = "White")
+        self.BulbButton.pack(side = LEFT, fill = X)
+
+    def CharClick():
+        print ("You like Charmander!")
+        CharPhoto =  ImageTk.PhotoImage(file='para.jpg')
+        ChLabel = Label(root, image = CharPhoto)
+        ChLabel.pack()
+
+
+k = PokemonClass(root)
+root.mainloop()
